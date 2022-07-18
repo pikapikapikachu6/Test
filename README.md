b7977bdeae0f19b3aa48a00b5d80f25d8439a20b
pikapikapikachu6 <68633954+pikapikapikachu6@users.noreply.github.com>
2022-07-18 16:49:07 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -27,3 +27,58 @@ Run dev server
 ```
 npm run dev
 ```
+
+
+
+|File|Description|
+|---|---|
+|[/data/*](./SRPC/data.md)|Include get and update data operations|
+|[/permission/*](./SRPC/permission.md)|Include add, get and delete permission operations|
+
+
+
+```srpc.Z.get(jwt, _id, data = { '': 1 })```: A gets B’s data.
+
+**parameter:**
+- ```jwt```: current user, A’s jwt.  
+- ```_id```: B’s “_id”.  
+- ```data```: the data A wants to get from B. 
+(Ps:      
+     - '': all data need to be get;  
+     - 0: do not query; >=1: query) 
+
+**rules:**
+1. A doesn’t have access to read B’s data, return “false”.
+2. B doesn‘t exist, return “undefined”.
+3. No B’s data, return “[]”.
+
+**test scene description:**
+- for rules1: A = user "root", B = user "test"
+1. A has access to read B's data, ``` data = { '': 0 }```
+expected outcome: return false
+
+2. A has access to read B's data, ``` data = { '': 1 }```
+
+- for rules2:
+
+- for rules3:
+
+
+srpc.Z.update
+User A update user B data 
+
+srpc.Z.template
+
+
+
+srpc.Z.getPermission
+
+
+srpc.Z.delPermission
+
+
+srpc.Z.addPermission
+
+
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,160 @@
 <script setup>
-let cot = $ref(0)
+import srpc from '../component/srpc.js'
+srpc('http://localhost:11111/')
+let jwt = 'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InJUR0FiU0tVZnVwZERlUmEifQ.eyJpZCI6InJvb3QiLCJuYW1lIjoi54Gv54Gr6JGz6JWkIiwiaXNzIjoiYWF1dGgiLCJpYXQiOjE2NTc0NTYxMzI4MTgsImF1ZCI6Inh5enNhcyJ9.tKL9-tMvsfJqPLKrlqXW1D-g6GGjQWkxKf8sBEBSOQk6zOld0U8-lSXA4wNzTmbX0sCMaeZrSBNmZ63sLfrulaK9QJTm7KKr2Is-SdRrOQlr1KcjdmjxHZMq_GkGRnwnWJkHp6DFKG5d-YewhOjR44nLk69dm8DJA3nmP4WKYre-ZqyHHzYS0e-qKq3g48b5RCQmW3KpZ6_uqA83VeQ7Dgsf9M9bgJgj8Xe8FPbqLEiRA5ChTfnT_RuLBr_7jF0qLgXxqA4xBMIJyRiDUIYkPQlgMMixCgpEPt0GEU1IQ1Ex7zj2pDBKMbuu7gCSin-AUiTWu6RdqHaEcGGwBugsiQ'
 
-function inc () {
-  cot++
+async function test_get_all() {
+  await test_get_1_0()
+  await test_get_1_1()
+  await test_get_1_2()
+  await test_get_0()
+  await test_get_1()
+  await test_get_2()
 }
+async function test_get_1_0() {
+  console.log('test_get_1_0')
+  const result = await srpc.Z.get(jwt, 'test', { '': 0 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_1_1() {
+  console.log('test_get_1_1')
+  const result = await srpc.Z.get(jwt, 'test', { '': 1 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_1_2() {
+  console.log('test_get_1_2')
+  const result = await srpc.Z.get(jwt, 'test', { '': 2 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_0() {
+  console.log('test_get_0')
+  const result = await srpc.Z.get(jwt, 'test2', { '': 0 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_1() {
+  console.log('test_get_1')
+  const result = await srpc.Z.get(jwt, 'test2', { '': 1 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_2() {
+  console.log('test_get_2')
+  const result = await srpc.Z.get(jwt, 'test2', { '': 2 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_part_base() {
+  // await test_get_base_0()
+  await test_get_base_1()
+  // await test_get_base_2()
+}
+
+async function test_get_base_0() {
+  console.log('test_get_base_0')
+  const result = await srpc.Z.get(jwt, 'test2', { '基础信息': 0 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_base_1() {
+  console.log('test_get_base_1')
+  const result = await srpc.Z.get(jwt, 'test', { '基础信息': 1 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_base_2() {
+  console.log('test_get_base_2')
+  const result = await srpc.Z.get(jwt, 'test2', { '基础信息': 2 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_part_other() {
+  await test_get_other_0()
+  await test_get_other_1()
+  await test_get_other_2()
+}
+
+async function test_get_other_0() {
+  console.log('test_get_other_0')
+  const result = await srpc.Z.get(jwt, 'test2', { '其他': 0 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_other_1() {
+  console.log('test_get_other_1')
+  const result = await srpc.Z.get(jwt, 'test2', { '其他': 1 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_other_2() {
+  console.log('test_get_other_2')
+  const result = await srpc.Z.get(jwt, 'test', { '其他': 2 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_part_uid() {
+  await test_get_uid_0()
+  await test_get_uid_1()
+  await test_get_uid_2()
+}
+
+async function test_get_uid_0() {
+  console.log('test_get_uid_2')
+  const result = await srpc.Z.get(jwt, 'test2', { '基础信息.uid': 2 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_uid_1() {
+  console.log('test_get_uid_2')
+  const result = await srpc.Z.get(jwt, 'test2', { '基础信息': { 'uid': 2 } })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_get_uid_2() {
+  console.log('test_get_uid_2')
+  const result = await srpc.Z.get(jwt, 'test', { 'uid': 2 })
+  console.log(result)
+  console.log('finish')
+}
+
+async function test_getPermission() {
+  console.log('test_getPermission')
+  const result = await srpc.Z.getPermission(jwt,'root')
+  console.log(result)
+  console.log('finish')
+}
+
+
+
 </script>
 
 <template>
-  <h1>Home</h1>
-  <p>cot: {{ cot }}</p>
-  <button @click="inc">Click Me</button>
+  <h1> Test v3-local </h1>
+  <P> </P>
+  <button @click="test_get_all()">test get all</button>
+  <br>
+  <button @click="test_get_part_base()">test get base</button>
+  <br>
+  <button @click="test_get_part_other()">test get 其他</button>
+  <br>
+  <button @click="test_get_part_uid()">test get uid</button>
+  <br>
+  <button @click="test_getPermission()">test getPermission</button>
 </template>
